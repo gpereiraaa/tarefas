ba130254383147c444d7ec8eb0d884ea9a53fd7a
GUSTAVO PEREIRA LEITE DA SILVA <25132694@dsn127m1242313.cfp127aluno.local>
2025-05-21 16:55:03 -0300

    Classe funcionario e classe tarefa adicionadas e enum status feito

--- src/br/dev/gustavo/tarefas/Main.java
+++ src/br/dev/gustavo/tarefas/Main.java
@@ -1,14 +1,53 @@
 package br.dev.gustavo.tarefas;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+
+import br.dev.gustavo.tarefas.model.Tarefa;
 
 public class Main {
-
+	static String caminho = "/Users/25132694/tarefasDS1TA/tarefas";
 	public static void main(String[] args) {
 		
-		String caminho = "/Users/25132694/tarefasDS1TA/tarefas";
+		
+		Tarefa tarefa1 = new Tarefa("Lavar a louça");
+		
+		
+		//escreverArquivo();
+		//lerArquivo();
+		
+		
+		
+		
+	}
+	
+	private static void escreverArquivo() {
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		
+		try {
+			fw = new FileWriter(caminho, true);
+			bw = new BufferedWriter(fw);
+			
+			//Adicionando conteúdo ao arquivo
+			//
+			//bw.append("Gustavo");
+			bw.append("xpto\n");
+			bw.append("Senai Jandira\n");
+			bw.append("Programação de computadores\n");
+			
+			//Mandando escrever no arquivo
+			bw.flush();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	private static void lerArquivo() {
 		FileReader fr = null;
 		BufferedReader br = null;
 		
@@ -26,10 +65,6 @@ public class Main {
 		} catch (Exception erro) {
 			System.out.println("Ocorreu um erro!");
 		}
-		
-		
-		
-		
 	}
 
 }
---
+++ src/br/dev/gustavo/tarefas/model/Status.java
@@ -0,0 +1,8 @@
+package br.dev.gustavo.tarefas.model;
+
+public enum Status {
+	
+	NAO_INICIADO, EM_ANDAMENTO, ATRASADO, CONCLUIDO
+	
+
+}
