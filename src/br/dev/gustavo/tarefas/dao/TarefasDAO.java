e01e6163ed72019d225d03cd901e96de7c6efb89
GUSTAVO PEREIRA LEITE DA SILVA <25132694@dsn127m1242313.cfp127aluno.local>
2025-06-16 16:31:17 -0300

    teste

--- src/br/dev/gustavo/tarefas/dao/TarefasDAO.java
+++ src/br/dev/gustavo/tarefas/dao/TarefasDAO.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import br.dev.gustavo.tarefas.model.Funcionario;
 import br.dev.gustavo.tarefas.model.Tarefa;
 
 public class TarefasDAO {
@@ -29,5 +33,38 @@ public class TarefasDAO {
 			System.out.println(e.getMessage());
 		}
 	}
-
+	
+	public void salvar() {
+		try {
+			bw.write(tarefa.toString());
+			bw.flush();
+			System.out.println(tarefa.getNome() + " gravado com sucesso!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Tarefa> getTarefas(){
+		
+		List<Tarefa> tarefas = new ArrayList<>();
+		
+		try {
+			String linha = "";
+			
+			while (linha !=null) {
+				linha = br.readLine();
+				if(linha !=null) {
+					String[] tarefasVetor = linha.split(",");
+					Tarefa tarefa = new Tarefa();
+					tarefa.setDataEntrega(tarefasVetor[0]);
+					tarefa.setDataInicio(tarefasVetor[1]);
+					tarefa.setStatus(tarefasVetor[2]);
+				}
+				return tarefas;
+			}
+			
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
